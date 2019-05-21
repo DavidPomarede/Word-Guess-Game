ef9af7b5f179df05f43b28d35145181b1219bbe2
doadet <davidpom@gmail.com>
2019-05-21 15:56:31 -0400

    working sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,11 @@ var wins = 0;
 var losses = 0;
 var list1 = [];
 var list2 = [];
-
+var audio1 = new Audio('assets/sounds/otr.mp3');
+var audio2 = new Audio('assets/sounds/guess.mp3');
+var audio3 = new Audio('assets/sounds/losses.mp3');
+var audio4 = new Audio('assets/sounds/miss.mp3');
+var audio5 = new Audio('assets/sounds/wins.mp3');
 
 var newGame = function () {
     game();
@@ -41,12 +45,14 @@ var game = function () {
                     currentWord[i] = userGuess;
                     document.getElementById('currentWord').innerHTML = currentWord.join('');
                     correctGuess++;
+                    audio2.play();
                 }
             }
         } else {
             if (lettersGuessed.indexOf(userGuess) == -1) {
                 lettersGuessed.push(userGuess);
                 guessesLeft--;
+                audio4.play();
                 list1.push(userGuess + " ");
                 for (j = 0; j < list1.length; j++) {
                     list2[j] = list1[j].toUpperCase();
@@ -59,6 +65,7 @@ var game = function () {
         if (guessesLeft === 0) {
             document.getElementById("message").innerHTML = 'You lost! the word was: ' + letters.join('');
             losses++;
+            audio3.play();
             document.getElementById('losses').innerHTML = 'losses: ' + losses;
             newGame();
         }
@@ -67,7 +74,7 @@ var game = function () {
             wins++;
             document.getElementById('wins').innerHTML = 'wins: ' + wins;
             document.getElementById('message').innerHTML = 'You won! You guessed: ' + letters.join('');
-
+            audio1.play();
             newGame();
         }
     }
