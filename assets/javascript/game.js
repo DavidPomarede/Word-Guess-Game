d36ae41d308fcd881192106cb062f92863627f9f
doadet <davidpom@gmail.com>
2019-05-21 12:27:27 -0400

    fixed wins problem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,7 +29,7 @@ var game = function () {
 
         var userGuess = event.key;
 
-        if ((letters.indexOf(userGuess) > -1) && (lettersGuessed.indexOf(userGuess) < 0)) {
+        if (letters.indexOf(userGuess) > -1) {
 
             for (var i = 0; i < letterCount; i++) {
                 if (letters[i] === userGuess) {
@@ -47,18 +47,18 @@ var game = function () {
             document.getElementById('guessedLetters').innerHTML = lettersGuessed + ' ';
         }
 
+        if (guessesLeft === 0) {
+            document.getElementById("message").innerHTML = 'You lost! the word was: ' + letters.join('');
+            losses++;
+            document.getElementById('losses').innerHTML = 'losses: ' + losses;
+            newGame();
+        }
+
         if (correctGuess === letters.length) {
             wins++;
             document.getElementById('wins').innerHTML = 'wins: ' + wins;
             document.getElementById('message').innerHTML = 'You won! You guessed: ' + letters.join('');
 
-            newGame();
-        }
-
-        if (guessesLeft === 0) {
-            document.getElementById("message").innerHTML = 'You lost! the word was: ' + letters.join('');
-            losses++;
-            document.getElementById('losses').innerHTML = 'losses: ' + losses;
             newGame();
         }
     }
